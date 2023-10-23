4c68a134e0c4e0ee71d9ada239c252bb0024d743
Your Name <you@example.com>
2023-10-23 11:33:02 +0200

    01

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ OPENID_PROVIDERS = [
     {"name": "MyOpenID", "url": "https://www.myopenid.com"},
 ]
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
-# SQLALCHEMY_DATABASE_URI = 'mysql://username:password@mysqlserver.local/quickhowto'
+#SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:example@db/flask'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://scott:tiger@localhost:5432/myapp'
 # SQLALCHEMY_ECHO = True
 SQLALCHEMY_POOL_RECYCLE = 3
